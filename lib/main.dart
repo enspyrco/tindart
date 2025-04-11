b511864875628b8e1db19e0f62890a3ca1621f66
Nick Meinhold <nick@enspyr.co>
2025-04-11 19:45:44 +1000

    Add UI & deps for uploading robin's art

--- lib/main.dart
+++ lib/main.dart
@@ -1,18 +1,99 @@
-import 'package:flutter/material.dart';
+import 'dart:io' as io;
 
-void main() {
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:file_picker/file_picker.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_storage/firebase_storage.dart';
+import 'package:flutter/material.dart';
+import 'package:path/path.dart' as path;
+import 'package:tindart/firebase_options.dart';
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
+
   runApp(const MainApp());
 }
 
-class MainApp extends StatelessWidget {
+class MainApp extends StatefulWidget {
   const MainApp({super.key});
 
   @override
+  State<MainApp> createState() => _MainAppState();
+}
+
+class _MainAppState extends State<MainApp> {
+  String _progressString = 'waiting for selection';
+  List<PlatformFile> imageFiles = [];
+  double _progressNum = 0;
+  int _filesNum = 0;
+
+  Future<void> _pickFile() async {
+    setState(() {
+      _progressString = 'copying files into app cache...';
+    });
+
+    FilePickerResult? result = await FilePicker.platform.pickFiles(
+      allowMultiple: true,
+      type: FileType.custom,
+      allowedExtensions: ['jpg'],
+    );
+
+    setState(() {
+      _progressString = 'cache is ready';
+    });
+
+    for (final PlatformFile file in result!.files) {
+      String basename = path.basename(file.path!);
+      if (basename.substring(0, 7) == 'FB_IMG_') {
+        imageFiles.add(file);
+      }
+    }
+
+    setState(() {
+      _filesNum = imageFiles.length;
+      _progressString = 'Uploading $_filesNum files...';
+    });
+
+    for (final PlatformFile file in imageFiles) {
+      Reference ref = FirebaseStorage.instance
+          .ref()
+          .child('facebook')
+          .child(path.basename(file.path!));
+
+      ref.putFile(io.File(file.path!)).then((snapshot) {
+        if (snapshot.state == TaskState.success) {
+          FirebaseFirestore.instance
+              .collection('images')
+              .add({'filePath': snapshot.ref.fullPath, 'source': 'facebook'})
+              .then((docRef) {
+                setState(() {
+                  _progressNum += 1 / imageFiles.length;
+                  _progressString = 'Uploading ${--_filesNum} files...';
+                });
+              });
+        }
+      });
+    }
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
+    return MaterialApp(
       home: Scaffold(
-        body: Center(
-          child: Text('Hello World!'),
+        body: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceAround,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            Center(child: Text(_progressString)),
+            LinearProgressIndicator(value: _progressNum),
+            TextButton(
+              onPressed: () {
+                _pickFile();
+              },
+              child: Text('open'),
+            ),
+          ],
         ),
       ),
     );
