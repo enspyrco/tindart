df3b65a935cd96c9f3069dd4874fa4fa83d54f0f
Nick Meinhold <nick@enspyr.co>
2025-06-04 15:47:23 +1000

    Remove args

--- cloudrun/read_firestore/bin/read_firestore.dart
+++ cloudrun/read_firestore/bin/read_firestore.dart
@@ -1,44 +1,14 @@
-import 'package:args/args.dart';
 import 'package:googleapis/firestore/v1.dart';
 import 'package:read_firestore/read_firestore.dart';
 
-Future<void> main(List<String> arguments) async {
-  final parser = ArgParser()
-    ..addOption(
-      'collection',
-      abbr: 'c',
-      help: 'Firestore collection name',
-      mandatory: true,
-    )
-    ..addOption(
-      'output',
-      abbr: 'o',
-      help: 'Output CSV file path',
-      defaultsTo: 'output.csv',
-    )
-    ..addOption(
-      'project',
-      abbr: 'p',
-      help: 'Google Cloud project ID',
-      mandatory: true,
-    );
-
-  final args = parser.parse(arguments);
-
+Future<void> main() async {
   // Authenticate using GOOGLE_APPLICATION_CREDENTIALS
   final authClient = await getAuthClient();
   final firestore = FirestoreApi(authClient);
 
   try {
-    final documents = await listDocuments(
-      firestore,
-      args['project']!,
-      args['collection']!,
-    );
-    await exportToCsv(documents, args['output']!);
-    print(
-      'Successfully exported ${documents.length} documents to ${args['output']}',
-    );
+    // await exportToCsv(documents, 'output.csv');
+    // print('Successfully exported ${documents.length} documents to output.csv');
   } finally {
     authClient.close();
   }
